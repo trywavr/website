965e6dca72ae5804ae2decb40ec2fbfcbf50c659
Mike Solomon <mike@meeshkan.com>
2021-10-18 22:51:59 +0300

    Adds PS to build

---
+++ src/Plumbing/JSON.purs
@@ -0,0 +1,19 @@
+module WAGSI.Plumbing.JSON where
+
+import Prelude
+
+import Control.Monad.Error.Class (throwError)
+import Data.List.NonEmpty (NonEmptyList(..))
+import Data.List (List(..))
+import Data.Map (Map, lookup)
+import Data.Maybe (Maybe(..))
+import Data.NonEmpty ((:|))
+import Foreign (F, Foreign, ForeignError(..))
+import Simple.JSON as JSON
+
+readVariant :: forall v. String -> Map String v -> Foreign -> F v
+readVariant name mp i =
+  JSON.readImpl i
+    >>= \s -> case lookup s mp of
+      Just x -> pure x
+      Nothing -> throwError (NonEmptyList ((ForeignError $ "Could not parse " <> name <> ": " <> s) :| Nil))
---
+++ src/handoff.d.ts
@@ -0,0 +1,9 @@
+type DemoInitialized = void & {_: 'DemoInitialized'}
+type DemoStarted = void & {_: 'DemoStarted'}
+
+declare module 'handoff' {
+  export function initialize(): () => Promise<DemoInitialized>;
+  export function start(logger: (txt: string) => () => void): (demoInitialized: DemoInitialized) => () => Promise<DemoStarted>
+  export function send(demoInitialized: DemoInitialized): (event: any) => () => void
+  export function stop(demoStarted: DemoStarted): () => void
+}
