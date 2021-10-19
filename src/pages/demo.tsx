ed32e5681e1269ab008376bfddcf43a7dcea249c
Makenna Smutz <makennasmutz@gmail.com>
2021-10-19 08:01:36 +0300

    Step 0 animated text

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/dist/client/router'
 import React, { useState } from 'react'
+import { Step0 } from '../components/demo/step-0'
 
 const Demo = () => {
   const router = useRouter()
@@ -8,7 +9,7 @@ const Demo = () => {
   const [step, setStep] = useState<number>(stepQueryParams || 0)
   return (
     <div>
-      {step === 0 && <h1>Demo</h1>}
+      {step === 0 && <Step0 />}
       {step === 1 && <h1>Hello</h1>}
       <button onClick={() => { setStep(step + 1); router.push(`/demo?stepNumber=${step + 1}`) }}>Next</button>
     </div>
