ca94e80a571e6a249006ab8adb90b104049bab74
Mike Solomon <mike@meeshkan.com>
2021-10-25 15:13:22 +0300

    Itlz

--- src/utils/wags/handoff.d.ts
+++ src/utils/wags/handoff.d.ts
@@ -1,9 +1,19 @@
-type DemoInitialized = void & {_: 'DemoInitialized'}
-type DemoStarted = void & {_: 'DemoStarted'}
+type DemoInitialized = void & { _: 'DemoInitialized' };
+type DemoStarted = void & { _: 'DemoStarted' };
 
 declare module 'handoff' {
-  export function initialize(): Promise<DemoInitialized>;
-  export function start(logger: (txt: string) => () => void): (demoInitialized: DemoInitialized) => () => Promise<DemoStarted>
-  export function send(demoInitialized: DemoInitialized): (event: any) => () => void
-  export function stop(demoStarted: DemoStarted): () => void
+	export function initialize(): Promise<DemoInitialized>;
+	export function initializeAndStart(
+		logger: (txt: string) => () => void
+	): () => Promise<{
+		demoStarted: DemoStarted;
+		demoInitialized: DemoInitialized;
+	}>;
+	export function start(
+		logger: (txt: string) => () => void
+	): (demoInitialized: DemoInitialized) => () => Promise<DemoStarted>;
+	export function send(
+		demoInitialized: DemoInitialized
+	): (event: any) => () => void;
+	export function stop(demoStarted: DemoStarted): () => void;
 }
