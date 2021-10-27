520dd20a3768c66f939a77a4468f1ca2bf0b0a6e
Makenna Smutz <makennasmutz@gmail.com>
2021-10-27 17:39:37 +0200

    small tweaks

--- src/components/label.tsx
+++ src/components/label.tsx
@@ -1,8 +1,7 @@
 import { styled } from '../../stitches.config';
 import * as LabelPrimitive from '@radix-ui/react-label';
-import { Text } from '@components/index';
 
-export const Label = styled(LabelPrimitive.Root, Text, {
+export const Label = styled(LabelPrimitive.Root, {
 	display: 'inline-block',
 	verticalAlign: 'middle',
 	cursor: 'default',
